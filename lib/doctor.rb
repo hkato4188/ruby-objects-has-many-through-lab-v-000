682a219e38a5b24955f3c3741e165f8b770003ca
Hiroki Kato <hkato@sas.upenn.edu>
2018-07-23 14:47:26 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,23 +1,23 @@
-# class Doctor
-#   attr_accessor :name
+class Doctor
+  attr_accessor :name
 
-#   def initialize(name)
-#     @name = name
-#     @appointments = []
-#   end
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
 
-#   def appointments
-#     @appointments
-#   end
+  def appointments
+    @appointments
+  end
 
-#   def add_appointment(appointment)
-#     self.appointments << appointment
-#     appointment.doctor = self
-#   end
+  def add_appointment(appointment)
+    self.appointments << appointment
+    appointment.doctor = self
+  end
 
-#   def patients
-#     self.appointments.collect do |appointment|
-#       appointment.patient
-#     end
-#   end
-# end
+  def patients
+    self.appointments.collect do |appointment|
+      appointment.patient
+    end
+  end
+end
